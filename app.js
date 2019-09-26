6f3484b746a1b4f7e27f562cefbe754a0b49ff81
guifcoelho <45208838+guifcoelho@users.noreply.github.com>
2019-09-26 09:46:17 -0300

    Melhorias e segurança

--- app.js
+++ app.js
@@ -7,32 +7,29 @@ const app = express();
 
 // Trigger a backup
 app.get('/cloud-firestore-export', async (req, res) => {
+
+    const AppEngineHeader = req.get('X-Appengine-Cron');
+    if(AppEngineHeader) return res.status(403).send('Cron called outside App Engine').end();
+
     const auth = await google.auth.getClient({
         scopes: ['https://www.googleapis.com/auth/datastore']
     });
 
     const accessTokenResponse = await auth.getAccessToken();
     const accessToken = accessTokenResponse.token;
-
     const headers = {
         'Content-Type': 'application/json',
         Authorization: 'Bearer ' + accessToken
     };
 
-    // const outputUriPrefix = req.param('outputUriPrefix');
-    // if (!(outputUriPrefix && outputUriPrefix.indexOf('gs://') == 0)) {
-    //     res.status(500).send(`Malformed outputUriPrefix: ${outputUriPrefix}`);
-    // }
-
     // Construct a backup path folder based on the timestamp
-
     const projectId = process.env.GOOGLE_CLOUD_PROJECT;
     const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default):exportDocuments`;
 
     const timestamp = dateformat(Date.now(), 'yyyy-mm-dd-HH-MM-ss');
     const path = [
-        `gs://projeto-compras-bd4f9.appspot.com/automatic-firestore-backup/${timestamp}/all`,
-        `gs://projeto-compras-bd4f9.appspot.com/automatic-firestore-backup/${timestamp}/per_collection`
+        `gs://${projectId}.appspot.com/automatic-firestore-backup/${timestamp}/all`,
+        `gs://${projectId}.appspot.com/automatic-firestore-backup/${timestamp}/per_collection`
     ];
     const body = path.map(el => { return {outputUriPrefix: el} });
 
@@ -43,24 +40,24 @@ app.get('/cloud-firestore-export', async (req, res) => {
     try {
         const [respAll, respPerColl] = await Promise.all([
             axios.post(url, body[0], { headers: headers }),
-            axios.post(url, {...body[1], collectionIds}, { headers: headers })
+            collectionIds ? axios.post(url, {...body[1], collectionIds}, { headers: headers }) : null
         ]);
 
         const responseBody = JSON.stringify([respAll.data, respPerColl.data]);
-        res.status(200).send(responseBody).end();
+        return res.status(200).send(responseBody).end();
         
     } catch (e) {
         if (e.response) {
             console.warn(e.response.data);
         }
 
-        res.status(500).send('Could not start backup: ' + e).end();
+        return res.status(500).send('Could not start backup: ' + e).end();
     }
 });
 
 // Index page, just to make it easy to see if the app is working.
 app.get('/', (req, res) => {
-    res.status(200).send('[scheduled-backups]: Hello, world!').end();
+    res.status(200).send('[automatic-firestore-backup]: Hello, world!').end();
 });
 
 // Start the server
